a4ce2c21391aac3bb06e4d36cef24777c916c3bd
its-kayes <kayes.ek8@gmail.com>
2023-10-12 13:01:00 +0600

    structured !

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../page/auth/Login";
 import Signup from "../page/auth/Signup";
 import Home from "../page/home/Home";
+import Controller from "../page/controller/Controller";
 
 /* All of Routers */
 const routers = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/class-controller",
+    element: <Controller />,
   },
   {
     path: "/sign-in",
