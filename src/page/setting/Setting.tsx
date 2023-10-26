77852730638b25c6a1ab83eaa4d592390b9b572e
its-kayes <kayes.ek8@gmail.com>
2023-10-26 12:53:11 +0600

    Language And Time Section !

--- src/page/setting/Setting.tsx
+++ src/page/setting/Setting.tsx
@@ -82,6 +82,51 @@ export default function Setting() {
               </div>
             </div>
           </Box>
+
+          <Box
+            sx={{
+              width: "100%",
+              marginTop: "40px",
+              marginBottom: "20px",
+            }}
+          >
+            <div className="language-time-section">
+              <p
+                style={{
+                  fontSize: "20px",
+                  fontWeight: "bold",
+                  marginBottom: "20px",
+                }}
+              >
+                {" "}
+                Language & Time{" "}
+              </p>
+
+              <div className="language-section">
+                <div>
+                  <p className="language-title"> Select language </p>
+                  <input type="text" placeholder="English (UK)" />
+                </div>
+
+                <div
+                  style={{
+                    marginTop: "20px",
+                  }}
+                >
+                  <p className="language-title"> Time zone </p>
+                  <input type="text" placeholder="GMT+01:00" />
+                </div>
+              </div>
+
+              <div
+                style={{
+                  marginTop: "20px",
+                }}
+              >
+                <button className="save-btn"> Save all </button>
+              </div>
+            </div>
+          </Box>
         </Grid>
 
         <Grid item xs={4}>
