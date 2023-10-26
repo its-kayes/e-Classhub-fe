20b4ec7b1e44f40e1c098f8f703c6a67a7a64618
its-kayes <kayes.ek8@gmail.com>
2023-10-26 16:39:37 +0600

    Setting Main Component restructure !

--- src/page/setting/Setting.tsx
+++ src/page/setting/Setting.tsx
@@ -1,40 +1,12 @@
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-// import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-
-import "./Style.scss";
-import { UserImage } from "../../importer/importer";
-import { Divider } from "@mui/material";
 import GeneralInfo from "../../elements/components/GeneralInfo";
 import PasswordInfo from "../../elements/components/PasswordInfo";
 import SessionsInfo from "../../elements/components/SessionsInfo";
-
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: "center",
-//   color: theme.palette.text.secondary,
-// }));
-
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
+import SocialInfo from "../../elements/components/SocialInfo";
+import LanguageInfo from "../../elements/components/LanguageInfo";
+import PrimaryInfo from "../../elements/components/PrimaryInfo";
+import "./Style.scss";
 
 export default function Setting() {
   return (
@@ -46,49 +18,7 @@ export default function Setting() {
               width: "100%",
             }}
           >
-            <div className="primary-user-info-box">
-              <img src={UserImage} alt="" />
-              <div className="header-box">
-                <div className="title-box">
-                  <p
-                    style={{
-                      fontSize: "25px",
-                      fontWeight: "bold",
-                    }}
-                  >
-                    Emrul Kayes
-                  </p>
-
-                  <p
-                    style={{
-                      fontSize: "15px",
-                      color: "#aaa",
-                      paddingTop: "5px",
-                    }}
-                  >
-                    Software Engineer
-                  </p>
-                </div>
-
-                <Button
-                  component="label"
-                  variant="contained"
-                  startIcon={<CloudUploadIcon />}
-                  sx={{
-                    textTransform: "none",
-                    width: "150px",
-                    fontSize: "12px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: "8px",
-                  }}
-                >
-                  Change picture
-                  <VisuallyHiddenInput type="file" />
-                </Button>
-              </div>
-            </div>
+            <PrimaryInfo />
           </Box>
 
           <Box
@@ -99,149 +29,10 @@ export default function Setting() {
             }}
           >
             {/* Language & Time */}
-            <div className="language-time-section">
-              <p
-                style={{
-                  fontSize: "20px",
-                  fontWeight: "bold",
-                  marginBottom: "20px",
-                }}
-              >
-                {" "}
-                Language & Time{" "}
-              </p>
-
-              <div className="language-section">
-                <div>
-                  <p className="language-title"> Select language </p>
-                  <input type="text" placeholder="English (UK)" />
-                </div>
-
-                <div
-                  style={{
-                    marginTop: "20px",
-                  }}
-                >
-                  <p className="language-title"> Time zone </p>
-                  <input type="text" placeholder="GMT+01:00" />
-                </div>
-              </div>
-
-              <div
-                style={{
-                  marginTop: "20px",
-                }}
-              >
-                <button className="save-btn"> Save all </button>
-              </div>
-            </div>
+            <LanguageInfo />
 
             {/* Social accounts */}
-            <div className="social-section">
-              <p
-                style={{
-                  fontSize: "20px",
-                  fontWeight: "bold",
-                  marginBottom: "20px",
-                }}
-              >
-                Social accounts
-              </p>
-
-              <div className="account-box">
-                <div className="per-account-box">
-                  <div className="header-box">
-                    <div className="icon">
-                      <FacebookRoundedIcon />
-                    </div>
-                    <div className="title-box">
-                      <p>Facebook account</p>
-                      <p className="link">www.facebook.com/themesberg</p>
-                    </div>
-                  </div>
-
-                  <div className="btn">
-                    <button> Disconnected </button>
-                  </div>
-                </div>
-
-                <Divider
-                  sx={{
-                    marginTop: "15px",
-                    marginBottom: "15px",
-                  }}
-                />
-
-                <div className="per-account-box">
-                  <div className="header-box">
-                    <div className="icon">
-                      <TwitterIcon />
-                    </div>
-                    <div className="title-box">
-                      <p>Twitter account</p>
-                      <p className="link">www.twitter.com/themesberg</p>
-                    </div>
-                  </div>
-
-                  <div className="btn">
-                    <button> Disconnected </button>
-                  </div>
-                </div>
-
-                <Divider
-                  sx={{
-                    marginTop: "15px",
-                    marginBottom: "15px",
-                  }}
-                />
-
-                <div className="per-account-box">
-                  <div className="header-box">
-                    <div className="icon">
-                      <GitHubIcon />
-                    </div>
-                    <div className="title-box">
-                      <p>Github account</p>
-                      <p className="no-link">Not connected</p>
-                    </div>
-                  </div>
-
-                  <div className="not-connected-box">
-                    <button> Connect </button>
-                  </div>
-                </div>
-
-                <Divider
-                  sx={{
-                    marginTop: "15px",
-                    marginBottom: "15px",
-                  }}
-                />
-
-                <div className="per-account-box">
-                  <div className="header-box">
-                    <div className="icon">
-                      <LinkedInIcon />
-                    </div>
-                    <div className="title-box">
-                      <p>Linkedin account</p>
-                      <p className="no-link">Not connected</p>
-                    </div>
-                  </div>
-                  <div className="not-connected-box">
-                    <button> Connect </button>
-                  </div>
-                </div>
-              </div>
-
-              <div
-                style={{
-                  marginTop: "30px",
-                }}
-              >
-                <button className="save-btn"> Save all </button>
-              </div>
-            </div>
+            <SocialInfo />
 
             {/* General Info */}
             <GeneralInfo />
