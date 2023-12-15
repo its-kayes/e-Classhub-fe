c3ba3269e404ed0561d3f92b2fd1bc6cd8a75d22
its-kayes <kayes.ek8@gmail.com>
2023-12-16 01:50:06 +0600

    Add Members component with pending and joined student lists

--- src/page/per-class-room/Members.tsx
+++ src/page/per-class-room/Members.tsx
@@ -1,3 +1,79 @@
+import "./Members.scss";
+
 export default function Members() {
-  return <div> Members </div>;
+  return (
+    <div>
+      {/* // List of Pending student */}
+      <section className="pending-box">
+        <h3 className="title"> List of Pending student </h3>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Student Name</th>
+                <th>Student Email</th>
+                <th>Student Gender</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Nguyen Van A</td>
+                <td>mail here</td>
+                <td>123456789</td>
+                <td className="status-box">
+                  <button className="status-pending"> Pending </button>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Nguyen Van A</td>
+                <td>mail here</td>
+                <td>123456789</td>
+                <td className="status-box">
+                  <button className="status-pending"> Pending </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+      {/* // List of Joined student */}
+      <section className="joined-box">
+        <h3 className="title"> List of Joined student </h3>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Student Name</th>
+                <th>Student Email</th>
+                <th>Student Gender</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Nguyen Van A</td>
+                <td>mail here</td>
+                <td>123456789</td>
+                <td className="status-box">
+                  <button className="status-joined"> Joined </button>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Nguyen Van A</td>
+                <td>mail here</td>
+                <td>123456789</td>
+                <td className="status-box">
+                  <button className="status-joined"> Joined </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </div>
+  );
 }
