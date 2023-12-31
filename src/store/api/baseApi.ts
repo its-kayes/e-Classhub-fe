35476e978c00b881f2142d5eeb2c1987b8fa3d9f
its-kayes <kayes.ek8@gmail.com>
2023-12-31 10:21:42 +0600

    Update base URL!

--- src/store/api/baseApi.ts
+++ src/store/api/baseApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: `https://api.eclasshub.kayes.dev/api/v1`,
-    baseUrl: `http://localhost:3000/api/v1`,
+    baseUrl: `https://api.eclasshub.kayes.dev/api/v1`,
+    // baseUrl: `http://localhost:3000/api/v1`,
   }),
   endpoints: () => ({}),
   tagTypes: ["User", "Classroom", "People", "Tracker", "Announcement"],
